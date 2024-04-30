7d2d53332ca084ea5667f6c30d2e58318611d713
Ebube-Ochemba <ebube17@gmail.com>
2024-04-30 14:24:25 +0100

    final manifest

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,16 +1,25 @@
-# install and configure nginx
+# Pupper manifet to install and configure nginx
+
+
+# Update package repositories
 exec {'update':
   command => '/usr/bin/apt-get update',
 }
+
+# Ensure Nginx service is installed
 -> package { 'nginx':
   ensure => installed,
 }
--> file_line { 'header_served_by':
+
+# Custom HTTP response header
+-> file_line { 'add X-Served-By header':
   path  => '/etc/nginx/sites-available/default',
   match => '^server {',
   line  => "server {\n\tadd_header X-Served-By \"${hostname}\";",
   multiple => false,
 }
--> exec {'run':
+
+# Restart Nginx
+-> exec {'restart nginx':
   command => '/usr/sbin/service nginx restart',
 }

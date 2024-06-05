647541cf9ac9a50c0267236983f04700c2726b60
Ebube-Ochemba <ebube17@gmail.com>
2024-06-05 05:16:33 +0100

    almost correctr

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
-"""Queries the Reddit API and returns the number of subscribers
-for a given subreddit."""
+"""number of subscribers"""
 
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Returns the number of subscribers or 0 if the subreddit is invalid.
+    """Queries the Reddit API and returns the number of subscribers
+    for a given subreddit.
     """
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {'User-Agent': 'MyRedditClient/1.0 (by /u/bane116)'}
 
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 200:
-        data = response.json().get('data').get('subscribers')
+    resp = requests.get(url, headers=headers, allow_redirects=False)
+    if resp.status_code == 200:
+        data = resp.json().get('data').get('subscribers')
         return data if data else 0
     else:
         return 0

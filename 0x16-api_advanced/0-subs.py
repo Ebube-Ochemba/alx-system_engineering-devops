c568ecb18156f468942b1df34fe91a001bfdc5f7
Ebube-Ochemba <ebube17@gmail.com>
2024-06-05 05:00:02 +0100

    correction

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,17 +8,13 @@ import requests
 def number_of_subscribers(subreddit):
     """Returns the number of subscribers or 0 if the subreddit is invalid.
     """
-    url = f'https://www.reddit.com/r/{subreddit}/about.json'
-    headers = {
-        'User-Agent': 'MyRedditAPI/0.1'
-    }
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {'User-Agent': 'bane116'}
 
-    try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        if response.status_code == 200:
-            data = response.json()
-            return data['data']['subscribers']
-        else:
-            return 0
-    except requests.RequestException:
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
+        data = response.json()
+        return data['data']['subscribers']
+    else:
         return 0
+    

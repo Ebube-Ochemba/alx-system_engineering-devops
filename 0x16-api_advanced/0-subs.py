3265af04b1cd19d27051a0d14bcd5dea14a0705b
Ebube-Ochemba <ebube17@gmail.com>
2024-06-05 05:32:03 +0100

    last check

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,7 +8,7 @@ def number_of_subscribers(subreddit):
     """Queries the Reddit API and returns the number of subscribers
     for a given subreddit.
     """
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
     headers = {'User-Agent': 'API Advanced (by /u/alx)'}
 
     response = requests.get(url, headers=headers, allow_redirects=False)

dbe8dfd865f1e3bafbb5a1859f4777dfe525a4c4
Ebube-Ochemba <ebube17@gmail.com>
2024-06-05 05:26:59 +0100

    last try

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,15 +8,12 @@ def number_of_subscribers(subreddit):
     """Queries the Reddit API and returns the number of subscribers
     for a given subreddit.
     """
-    if subreddit is None or type(subreddit) is not str:
-        return 0
-
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {'User-Agent': 'MyRedditClient/1.0 (by /u/bane116)'}
+    headers = {'User-Agent': 'API Advanced (by /u/alx)'}
 
-    resp = requests.get(url, headers=headers, allow_redirects=False)
-    if resp.status_code == 200:
-        data = resp.json().get('data').get('subscribers')
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        data = response.json().get('data').get('subscribers')
         return data if data else 0
     else:
         return 0

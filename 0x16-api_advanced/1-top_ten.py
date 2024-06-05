9635da52fc48e76043f0db087ee15f6f1c01d34d
Ebube-Ochemba <ebube17@gmail.com>
2024-06-06 00:37:02 +0100

    syntax

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -6,11 +6,13 @@ import requests
 
 def top_ten(subreddit):
     """Print the titles of the first 10 hot posts for a given subreddit."""
-    url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
+    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
     headers = {'User-Agent': 'API Advanced (by /u/alx)'}
+    params = {'limit': 100}
 
     try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
+        response = requests.get(url, headers=headers, params=params,
+                                allow_redirects=False)
         if response.status_code == 200:
             data = response.json().get('data', {}).get('children', [])
             for post in data:

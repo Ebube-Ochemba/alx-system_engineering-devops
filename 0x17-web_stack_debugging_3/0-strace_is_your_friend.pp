c60d5aefa5002c370e12b7e521ce8e6f7314644f
Ebube-Ochemba <ebube17@gmail.com>
2024-06-19 16:11:30 +0100

    second try

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,32 +1,9 @@
-# 0-strace_is_your_friend.pp
-# Puppet manifest to fix typo errors in .phpp files
+# Puppet manifest to fix the typo error in wp-settings.php
 
-# Ensure the directory exists
-file { '/var/www/html':
-  ensure => 'directory',
-  owner  => 'www-data',
-  group  => 'www-data',
-  mode   => '0755',
-}
-
-# Search for .phpp and replace with .php
-exec { 'fix-phpp-typo':
-  command => "grep -rl 'phpp' /var/www/html/wp-settings.php | xargs sed -i 's/phpp/php/g'",
-  path    => ['/bin', '/usr/bin'],
-  onlyif  => "grep -rl 'phpp' /var/www/html/wp-settings.php",
-}
-
-# Notify the service to restart Apache after fixing the typo
-service { 'apache2':
-  ensure    => 'running',
-  enable    => true,
-  subscribe => Exec['fix-phpp-typo'],
-}
-
-# Reload Apache service to apply changes
-exec { 'reload-apache2':
-  command     => '/usr/sbin/service apache2 reload',
-  path        => ['/bin', '/usr/bin', '/usr/sbin'],
-  refreshonly => true,
-  subscribes  => Exec['fix-phpp-typo'],
+# Ensure the file is present and correct the typo error
+file_line { 'correct_phpp_typo':
+  ensure => 'present',
+  path   => '/var/www/html/wp-settings.php',
+  match  => 'phpp',
+  line   => 'corrected line with .php',  # Provide the correct line with the right '.php' extension
 }

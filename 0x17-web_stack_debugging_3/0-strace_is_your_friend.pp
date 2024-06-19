4ccc64dd2f7fe18ff79110f148784ac979c2f44d
Ebube-Ochemba <ebube17@gmail.com>
2024-06-19 16:23:23 +0100

    refactor

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,15 +1,19 @@
 # Puppet manifest to fix the typo error in wp-settings.php
 
-
 $typo_file = '/var/www/html/wp-settings.php'
 
 # Ensure the file is present
 file { $typo_file:
   ensure => 'present',
+  mode   => '0644',  # Ensure proper permissions
+  owner  => 'www-data',
+  group  => 'www-data',
 }
 
-# Correct the typo error
+# Correct the typo error using an exec resource
 exec { 'correct_phpp_typo':
   command => "sed -i 's/class-wp-locale.phpp/class-wp-locale.php/g' ${typo_file}",
-  path    => ['/bin', '/usr/bin']
+  path    => ['/bin', '/usr/bin'],
+  onlyif  => "grep 'class-wp-locale.phpp' ${typo_file}",  # Only run if the typo exists
+  require => File[$typo_file],  # Ensure the file resource is managed before executing the command
 }

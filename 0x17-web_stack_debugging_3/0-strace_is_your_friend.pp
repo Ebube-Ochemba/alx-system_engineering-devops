b3bba890ae2041563a7161ca36f6cb0c2755d9a2
Ebube-Ochemba <ebube17@gmail.com>
2024-06-19 16:19:47 +0100

    correction

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,9 +1,15 @@
 # Puppet manifest to fix the typo error in wp-settings.php
 
-# Ensure the file is present and correct the typo error
-file_line { 'correct_phpp_typo':
+
+$typo_file = '/var/www/html/wp-settings.php'
+
+# Ensure the file is present
+file { $typo_file:
   ensure => 'present',
-  path   => '/var/www/html/wp-settings.php',
-  match  => 'phpp',
-  line   => 'corrected line with .php',  # Provide the correct line with the right '.php' extension
+}
+
+# Correct the typo error
+exec { 'correct_phpp_typo':
+  command => "sed -i 's/class-wp-locale.phpp/class-wp-locale.php/g' ${typo_file}",
+  path    => ['/bin', '/usr/bin']
 }

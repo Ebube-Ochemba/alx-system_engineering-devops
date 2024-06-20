f8b336f17fb9a55e5daed3e33c83a30c7de76bcc
Ebube-Ochemba <ebube17@gmail.com>
2024-06-20 18:44:41 +0100

    puppet-lint

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -2,17 +2,17 @@
 
 # Increase the open file limit for Nginx from 15 to 4096 in /etc/default/nginx
 exec { 'increase_ulimit_nginx':
-  command   => 'sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 4096\"/" /etc/default/nginx',
-  provider  => shell,
-  onlyif    => 'grep -q "ULIMIT=\"-n 15\"" /etc/default/nginx',
-  logoutput => true,
-  notify    => Exec['restart_nginx'], # Notify the restart exec resource upon change
+    command   => 'sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 4096\"/" /etc/default/nginx',
+    provider  => shell,
+    onlyif    => 'grep -q "ULIMIT=\"-n 15\"" /etc/default/nginx',
+    logoutput => true,
+    notify    => Exec['restart_nginx'], # Notify the restart exec resource upon change
 }
 
 # Restart the Nginx service to apply the new configuration
 exec { 'restart_nginx':
-  command   => 'service nginx restart',
-  provider  => shell,
-  refreshonly => true, # Only run this command if notified
-  logoutput => true,
+    command     => 'service nginx restart',
+    provider    => shell,
+    refreshonly => true, # Only run this command if notified
+    logoutput   => true,
 }

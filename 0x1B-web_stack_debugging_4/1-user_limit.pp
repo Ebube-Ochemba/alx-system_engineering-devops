7471dcef5f5300ca11a8acc39ae4b3fb43992afc
Ebube-Ochemba <ebube17@gmail.com>
2024-06-20 20:01:37 +0100

    done

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,23 @@
+# Puppet manifest to set open file limits for the system
+# This manifest will correct the hard and soft limits for the maximum number of open files.
+
+# Ensure the /etc/security/limits.conf file exists
+file { '/etc/security/limits.conf':
+  ensure => file,
+  mode   => '0644',
+}
+
+# Fix the limits for the 'nofile' descriptor in /etc/security/limits.conf
+exec { 'Correct file descriptor limits':
+  command => 'sudo sed -i -e "s/nofile 5/nofile 2048/" -e "s/nofile 4/nofile 2048/" /etc/security/limits.conf',
+  onlyif  => 'grep -qE "nofile [45]" /etc/security/limits.conf',
+  path    => '/usr/bin:/bin',
+  require => File['/etc/security/limits.conf'],
+}
+
+# Reload the limits to apply changes
+exec { 'reload_limits':
+  command     => 'ulimit -n 30000',
+  refreshonly => true,
+  subscribe   => Exec['Correct file descriptor limits'],
+}
